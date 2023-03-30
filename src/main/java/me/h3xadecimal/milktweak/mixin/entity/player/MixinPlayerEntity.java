b6bb29b26f184ee4dbf50a8d4ba0d7868b3cbf9c
H3xadecimal <h3xadecimal@afternode.cn>
2023-03-30 21:44:01 +0800

    修复注入失败问题

--- src/main/java/me/h3xadecimal/milktweak/mixin/entity/player/MixinPlayerEntity.java
+++ src/main/java/me/h3xadecimal/milktweak/mixin/entity/player/MixinPlayerEntity.java
@@ -1,9 +1,11 @@
 package me.h3xadecimal.milktweak.mixin.entity.player;
 
+import com.mojang.authlib.GameProfile;
 import me.h3xadecimal.milktweak.SpecialPlayerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,12 +15,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity {
 
-    @Shadow public abstract ITextComponent getName();
+    @Shadow public abstract GameProfile getGameProfile();
 
     @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
     public void getDisplayName(CallbackInfoReturnable<ITextComponent> cir) {
-        if (SpecialPlayerManager.INSTANCE.hasDisplayName(getName().getString())) {
-            cir.setReturnValue(new StringTextComponent(SpecialPlayerManager.INSTANCE.getDisplayName(getName().getString())));
+        if (SpecialPlayerManager.INSTANCE.hasDisplayName(getGameProfile().getName())) {
+            cir.setReturnValue(new StringTextComponent(SpecialPlayerManager.INSTANCE.getDisplayName(getGameProfile().getName())));
         }
     }
 }

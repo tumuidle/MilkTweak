2c70f5ae0403ca3ef840d9bd16f808fc726c0893
H3xadecimal <h3xadecimal@afternode.cn>
2023-08-03 23:12:41 +0800

    移除无用注入

--- src/main/java/me/h3xadecimal/milktweak/mixin/MixinPlayerEntity.java
+++
@@ -1,8 +0,0 @@
-package me.h3xadecimal.milktweak.mixin;
-
-import net.minecraft.entity.player.PlayerEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PlayerEntity.class)
-public class MixinPlayerEntity {
-}
